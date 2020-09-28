01e4f42cc9507fbfad62b0f4e94f7cedc9d34d55
Carlos Aparicio <carlos_non86@msn.com>
2020-09-28 15:07:54 +0200

    Get Post and Page Singles from :slug

--- api/Pages/Page.php
+++ api/Pages/Page.php
@@ -22,11 +22,11 @@ function redirect_rest_pages()
 function get_rest_pages($request)
 {
     // fetch params
-    $pid = $request['page_id'];
+    $pslug = $request['page_slug'];
 
     // fetch db
     $results = new WP_Query(array(
-        'page_id' => $pid,
+        'pagename' => $pslug,
     ));
 
     // construct response
@@ -40,7 +40,7 @@ function get_rest_pages($request)
 add_action('rest_api_init', function () {
     register_rest_route(
         'esd/v1',
-        'page/(?P<page_id>\d+)',
+        'page/(?P<page_slug>\d+)',
         array(
             'methods' => 'GET',
             'callback' => 'get_rest_pages'
--- api/Post/Post_Home.php
+++ api/Post/Post_Home.php
@@ -13,11 +13,11 @@
 function get_rest_post_home($request)
 {
     // fetch params
-    $pid = $request['post_id'];
+    $pslug = $request['post_slug'];
 
     // fetch db
     $results = get_posts(array(
-        'p' => $pid, 
+        'post_name' => $pslug, 
         'post_type' => 'any'
     ));
 
@@ -25,7 +25,7 @@ function get_rest_post_home($request)
     if ($results[0]->post_type !== 'page') {
         $response = new esd_BE_Post_Home_Single($results[0]);
     } else {
-        wp_redirect(esd_BE__BasicData::$api_root . 'page/' . $pid);
+        wp_redirect(esd_BE__BasicData::$api_root . 'page/' . $pslug);
         exit;
     }
 
@@ -35,7 +35,7 @@ function get_rest_post_home($request)
 add_action('rest_api_init', function () {
     register_rest_route(
         'esd/v1',
-        'posts/(?P<post_id>\d+)',
+        'posts/(?P<post_slug>\d+)',
         array(
             'methods' => 'GET',
             'callback' => 'get_rest_post_home'
