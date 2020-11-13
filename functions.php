ec8bf115c3c98643ebff4932764ee2abc0f3e4b0
Carlos Aparicio <carlos_non86@msn.com>
2020-11-13 12:27:40 +0100

    convenios endpoint

--- functions.php
+++ functions.php
@@ -40,6 +40,7 @@ require dirname(__FILE__) . '/api/Portfolio/Portfolio_List.php';
 require dirname(__FILE__) . '/api/Portfolio/Portfolio_Single.php';
 
 require dirname(__FILE__) . '/api/Companies/Company_List.php';
+require dirname(__FILE__) . '/api/Convenios/Convenios_List.php';
 
 require dirname(__FILE__) . '/api/Search/Search.php';
 require dirname(__FILE__) . '/api/Menu/Menu.php';
