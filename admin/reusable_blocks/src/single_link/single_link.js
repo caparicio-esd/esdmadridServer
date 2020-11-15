d09407622cc56085be39c9963c07570f3bf57bdc
caparicio-esd <caparicio@esdmadrid.es>
2020-11-15 09:35:51 +0100

    Single link gutenberg

--- admin/reusable_blocks/src/single_link/single_link.js
+++ admin/reusable_blocks/src/single_link/single_link.js
@@ -1,61 +1,73 @@
-const { registerBlockType } = wp.blocks; 
-const { RichText } = wp.editor;
-const { TextControl, ExternalLink } = wp.components;
+const { registerBlockType } = wp.blocks;
+const { RichText } = wp.blockEditor;
 import './single_link.scss';
 
 
+/**
+ *
+ * Editor React Component
+ */
 const SingleLinkEditor = ({ attributes, setAttributes }) => {
-
-    const changeTitle = e => {
-
+    const changeTitleHandler = (value) => {
+        setAttributes({ title: value });
+    };
+    const changeLinkHandler = (value) => {
+        setAttributes({ link: value });
     };
 
-    const changeLink = e => {
-
-    };
-
-
-    return ([
-        <div className="cta-container">
-            <label style={{fontWeight: 'bold'}}>Link de descarga</label>
-            <RichText placeholder="Escribe el link" style={{padding: '.5rem 0', borderBottom: '1px solid #aaa'}} />
-            <p><small>Este link se meterá en el set de links de descargas al final de cada sección.</small></p>
-        </div>
-    ]);
+    return [
+        <div className="single_link">
+            <label>Link de descarga</label>
+            <RichText
+                placeholder="Escribe el title"
+                value={attributes.title}
+                onChange={changeTitleHandler}
+            />
+            <RichText
+                placeholder="Escribe el link"
+                value={attributes.link}
+                onChange={changeLinkHandler}
+            />
+            <p>
+                <small>
+                    Este link se meterá en el set de links de descargas al final de cada sección.
+                </small>
+            </p>
+        </div>,
+    ];
 };
 
+/**
+ *
+ * Saving React Component
+ *
+ * Sigo intentado...
+ */
+const SingleLinkSave = ({ attributes }) => {
+    return (
+        <div className="single_link">
+            <a href={attributes.link}>{ attributes.title }</a>
+        </div>
+    );
+};
 
-
-registerBlockType('esd/anchor', {
-    // Built in attributes
-    title: "Anchor",
-    description: "Block to generate anchor",
-    icon: "format-image",
-    category: "text",
-
-    // custom attributes
+registerBlockType('esd/single-link', {
+    title: 'Single Link',
+    description: 'Block to generate a single link',
+    icon: 'format-image',
+    category: 'text',
     attributes: {
         title: {
             type: 'string',
-            source: 'html', 
-            selector: 'h5'
-        }, 
+        },
         link: {
             type: 'string',
-            source: 'html', 
-            selector: 'p'
-        } 
+        },
     },
-
-    // custom functions
-
-
-    // built-in functions
     edit(props) {
-        return <SingleLinkEditor {...props} />
+        return <SingleLinkEditor {...props} />;
     },
-
-    save({ attributes }) {
-        return <div>hola</div>
-    }
-})
+    save(props) {
+        return <SingleLinkSave {...props} />;
+    },
+});
--- admin/reusable_blocks/src/single_link/single_link.php
+++ admin/reusable_blocks/src/single_link/single_link.php
@@ -5,7 +5,7 @@ function register_single_anchor()
     wp_enqueue_script(
         'esd_single_link',
         get_template_directory_uri() . '/admin/reusable_blocks/dist/single_link/single_link.js',
-        array('wp-blocks', 'wp-i18n', 'wp-editor'),
+        array('wp-blocks', 'wp-i18n', 'wp-editor', 'wp-block-editor'),
         false,
         true
     );
